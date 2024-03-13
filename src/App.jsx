a58e3456afec191173b0008f9087c3317c5c4457
imran2024bd <imranitbd34@gmail.com>
2024-03-13 12:57:38 +0600

    39-5 (Advanced) Understand The Concept Of UseEffect done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Counter from './Counter';
 import Team from './Team';
+import Users from './Users';
 
 function App() {
 
@@ -21,6 +22,9 @@ const addToFive = (num) => {
     <>
       
       <h3>React Core Concepts 2</h3>
+      <Users></Users>
+
+      
       <Team></Team>
       <Counter></Counter>
 
---
+++ src/Users.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react"
+
+export default function Users () {
+    const [users , setusers ]= useState([]);
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => setusers(data))
+        // .then(data => console.log(data))
+    } , [])
+
+    return (
+        <div>
+            <h3> Users : {users.length}</h3>
+        </div>
+    )
+}
+
+
+/**
+ * 1. declare a state to hold the data
+ *  2. useEffect with call back and dependency array
+ * 3.  use fetch to load data
+ */
